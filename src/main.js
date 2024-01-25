066f84bf57a01e8576111900e9e2767779bb9423
josefinek <josefine@kafer.com>
2024-01-25 18:14:01 +0100

    lesson 1 on this

--- src/main.js
+++ src/main.js
@@ -7,18 +7,21 @@ function render(data, targetElement) {
     <div class="description">
         ${data.description}
     </div>
+    <div class="description">
+        ${data.price}
+    </div>
 </div>
-`
+`// links to the style.css say how the data should look
 }
 
 async function main() {
   const artList = document.getElementById("article-list")
 
-  const data = await jsonLoader("/level-01.json")
+  const data = await jsonLoader("/level-02.json") // links to the data base 
 
   for (let i = 0; i < data.length; i++) {
     render(data[i], artList)
-  }
+  } // cycles through the data base 
 }
 
 document.addEventListener("DOMContentLoaded", main)
